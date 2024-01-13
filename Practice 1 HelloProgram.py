3d5ba7db623b9b89d39cc36f29e67535cc2bc1e1
QAonkarvijapure <149368667+QAonkarvijapure@users.noreply.github.com>
2024-01-13 23:55:52 +0530

    1. HelloProgram.py

--- Practice 1 HelloProgram.py
+++ Practice 1 HelloProgram.py
@@ -1,15 +1,4 @@
-str = "Hello world" # string represents in "_" (double quote)
-print(str)   # print is function and () bracket is called as parenthisis
-print("hello_Onkar\n") #here "hello Onkar" is string
-print("Onkar\n") #"onkar" is substring is known as part of string
-# note: string is immutable(can not change)
-# single part() changes can not possible in string (immutable)
-# complete string can be changeble
-
-#errors FOUND
-#1st=print("hello Onkar"
-#    ^SyntaxError: '(' was never closed
-
-# 2nd=  print("hello Onkar)
-          #SyntaxError: unterminated string literal (detected at line 1)
-          
+str = "Hello world" 
+print(str)  
+print("hello_Onkar\n")  
+print("Onkar\n")            
